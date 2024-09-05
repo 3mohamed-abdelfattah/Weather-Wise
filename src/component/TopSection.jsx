82726041a85e3c70cf09806c5890e3a7cf353c12
MohamedMohamed <m01126165016@icloud.com>
2024-09-05 09:25:46 +0300

    Add Gif Photo

--- src/component/TopSection.jsx
+++ src/component/TopSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../assets/style/TopSecStyle.css'
+import IconGif from '../assets/img/rainy-girl.gif'
 
 export default function TopSection(props) {
     return (
@@ -12,7 +13,7 @@ export default function TopSection(props) {
                 <p className="temperature">{props.temperature ? props.temperature : "18°C"}</p>
             </div>
             <div className="weather-icon">
-                <img style={{ width: 200 }} src="https://s3-alpha-sig.figma.com/img/d6b0/8e40/6b03c91953a7c9f5bcd08d152a0f8491?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Q80xRsEpfivjGtdQDZA3tu~B5N22-qvEI80RNTFGIJ37RzFYXj86DRK-OdONLcGaFdw4vcmfeHIP3LnsEEQyCDfWGQVD8A~RN0~ZTZCRNq7f9YIr5HIfEwIxo647E7xh7JZHeWOZMQreHJCH0wpZzgZ5KJCX72YxFrz489gThmba-VR8FT~5wtW5cRN0LPyPUyqvQXY7Oi5TdDV4f~xuoyAIND0Ir2C-iByNO5xu6BrqWlwne0ynhg3j2dVwkIzXAp0Sl3BOwmjSLG0uY2R6slSjoUMO-HvgxslnqjN7NTI5w9G1nnsOKILdcgIIF4GoPNJ0w~GxySnBLkF8L9-HRQ__" alt="Icon" />
+                <img style={{ width: 200 }} src={IconGif} alt="Icon Gif" />
             </div>
         </div>
     )
